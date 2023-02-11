3cccb2004771b2fd99bb5ba48f358805eba98a29
Jesse Naldo <naldojesse@icloud.com>
2023-02-11 15:55:13 +0700

    implemented task creation, deletion, and updating

--- todolistprj/todo/views.py
+++ todolistprj/todo/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
 from . models import Task
 
 
@@ -18,3 +21,19 @@ class TaskDetail(DetailView):
     context_object_name = 'task'
     template_name = 'todo/task.html'
     
+# importing CreateView generic view for creating tasks
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    # fields = ['title', 'description' ]
+    success_url = reverse_lazy('task')
+    
+class TaskUpdate(UpdateView): 
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task")
+    
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy("task")
